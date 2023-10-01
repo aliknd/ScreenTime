1b43e8224ec2284cbeaa75bebd35036b1164e306
aliknd <a.kargaran.d@gmail.com>
2023-09-30 20:19:38 -1000

    second commit - usage stats update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:app_usage/app_usage.dart';
+import 'package:usage_stats/usage_stats.dart';
+import 'package:app_settings/app_settings.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() {
-    print("yek do se");
-    return _MyAppState();
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  List<AppUsageInfo> _infos = [];
+  List<String> _packageNames = [];
 
   @override
   void initState() {
     super.initState();
+    getUsageStats();
   }
 
   void getUsageStats() async {
-    try {
-      print("##################");
-      DateTime endDate = DateTime.now();
-      DateTime startDate = endDate.subtract(const Duration(days: 10));
-      List<AppUsageInfo> infoList = await AppUsage().getAppUsage(startDate, endDate);
-      print(infoList);
-      setState(() => _infos = infoList);
-      print("************");
-      // for (var info in infoList) {
-      //   print("information");
-      //   // print(info.toString());
-      //   print(info.appName);
-      // }
-    } on AppUsageException catch (exception) {
-      print("Exception");
-      // print(exception);
+    // Grant usage permission - opens Usage Settings
+    UsageStats.grantUsagePermission();
+
+    // Wait briefly to ensure permission is granted
+    await Future.delayed(Duration(seconds: 1));
+
+    // Check if permission is granted
+    bool? isPermission = await UsageStats.checkUsagePermission();
+
+    if (isPermission == null || !isPermission) {
+      // If permission is not granted or null, guide the user to enable it
+      AppSettings.openAppSettings();
+      return;
     }
+
+    DateTime endDate = DateTime.now();
+    DateTime startDate = DateTime(endDate.year, endDate.month, endDate.day, 0, 0, 0);
+
+    // Query usage stats
+    List<UsageInfo?>? usageStats = await UsageStats.queryUsageStats(startDate, endDate);
+
+    if (usageStats == null) {
+      // Handle the case where usageStats is null (e.g., no data available)
+      return;
+    }
+
+    // Extract package names from usage stats
+    _packageNames = usageStats
+        .where((info) => info != null && info.packageName != null)
+        .map((info) => info!.packageName!)
+        .toList();
+
+    // Update the UI with package names
+    setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Screen Timing'),
-          backgroundColor: Colors.green,
+          title: Text('App Usage Stats'),
         ),
         body: ListView.builder(
-            itemCount: _infos.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text(_infos[index].appName),
-                  trailing: Text(_infos[index].usage.toString()));
-            }),
-        floatingActionButton: FloatingActionButton(
-            onPressed: getUsageStats, child: Icon(Icons.file_download)),
+          itemCount: _packageNames.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_packageNames[index]),
+            );
+          },
+        ),
       ),
     );
   }
